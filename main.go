c1e0487b4e35a8ab1dae82e71cecbc25e5db0119
Andrew Wichmann <awichmann@mintel.com>
2021-06-15 15:49:39 -0500

    bump

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "mintel",
-			Version: "0.0.8",
+			Version: "0.0.10",
 			Rules: []tflint.Rule{
 				rules.NewAwsInstanceExampleTypeRule(),
 				rules.NewAwsS3BucketExampleLifecycleRuleRule(),
